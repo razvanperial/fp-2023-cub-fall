6665e0c1b62bdf420bfafdaf475cd7e88c699f5c
Ekaterina Verbitskaia <kajigor@gmail.com>
2023-11-01 14:08:38 +0100

    Lecture material

---
+++ stlc/test/Test/TypeCheck.hs
@@ -0,0 +1,14 @@
+module Test.TypeCheck where
+
+import Test.Tasty.HUnit (Assertion, (@?=))
+
+import Syntax
+import TypeCheck
+
+unit_typecheck = do
+    check (Abs "x" Bool (Var "x")) (Arrow Bool Bool)
+    check (If (App (Abs "x" Bool (Var "x")) (BoolLit True)) (BoolLit True) (BoolLit False)) Bool
+    check (Abs "f" (Arrow (TyVar "b") (TyVar "c")) (Abs "g" (Arrow (TyVar "a") (TyVar "b")) (Abs "x" (TyVar "a") (App (Var "f") (App (Var "g") (Var "x")))))) (Arrow (Arrow (TyVar "b") (TyVar "c")) (Arrow (Arrow (TyVar "a") (TyVar "b")) (Arrow (TyVar "a") (TyVar "c"))))
+  where
+    check term typ =
+      typeCheckEmpty term @?= Just typ
